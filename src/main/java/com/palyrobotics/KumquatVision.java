78b02af59b97a9d4642caaf8bac8fd83cc06af7d
Quintin Dwight <qhdwight@gmail.com>
2019-11-17 18:51:06 -0800

    Using OpenCV only instead of CS core

--- src/main/java/com/palyrobotics/KumquatVision.java
+++ src/main/java/com/palyrobotics/KumquatVision.java
@@ -1,23 +1,39 @@
 package com.palyrobotics;
 
+import edu.wpi.cscore.CameraServerJNI;
 import edu.wpi.cscore.CvSink;
 import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpiutil.RuntimeLoader;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.highgui.HighGui;
+import org.opencv.videoio.VideoCapture;
+
+import java.io.IOException;
 
 public class KumquatVision {
+
+    static {
+        var openCvLoader = new RuntimeLoader<>(Core.NATIVE_LIBRARY_NAME, RuntimeLoader.getDefaultExtractionRoot(), Core.class);
+        try {
+            openCvLoader.loadLibraryHashed();
+        } catch (IOException loadException) {
+            loadException.printStackTrace();
+        }
+    }
 
     public static void main(String... arguments) {
         new KumquatVision();
     }
 
-    private KumquatVision()
-    {
-        var camera = new UsbCamera("Vision", "/dev/video0");
-        var sink = new CvSink(camera.getName());
-        Mat inputFrame = new Mat();
-        while (camera.isConnected()) {
-            sink.setSource(camera);
-            sink.grabFrame(inputFrame);
+    private KumquatVision() {
+        var mat = new Mat();
+        var capture = new VideoCapture(0);
+        while (capture.isOpened()) {
+            if (capture.read(mat)) {
+            }
         }
+        capture.release();
+//        HighGui.destroyAllWindows();
     }
 }
